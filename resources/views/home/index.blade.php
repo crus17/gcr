92d9b334033f4dde69963139f41b571e10e27f19
Alfred Ekeuwei <ekeuwei@gmail.com>
2022-10-13 10:38:10 +0100

    Translate page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -150,7 +150,7 @@
 			<div style="height:20px; padding:0px; margin:0px; width: 100%;">
 			<iframe src="https://widget.coinlib.io/widget?type=horizontal_v2&theme=dark&pref_coin_id=1505&invert_hover=no" width="100%" height="36px" scrolling="auto" marginwidth="0" marginheight="0" frameborder="0" border="0" style="border:0;margin:0;padding:0;"></iframe>
             @if ($settings->translate_page !== "off")
-                <div id="google_translate_element"></div>
+                <div class="ml-4" id="google_translate_element"></div>
             @endif
 	   </div>
             <!-- Hero Section End -->   

